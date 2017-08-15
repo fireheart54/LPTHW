6fc5be085a045b704935ffc57c1921d0b80715a2
Fengbo Zhao <fireheart54@gmail.com>
2017-08-15 09:09:33 -0500

    added automated tools

--- gothonweb/tests/app_tests.py
+++ gothonweb/tests/app_tests.py
@@ -1,3 +1,22 @@
 from nose.tools import *
-from bin.app import app
-from tests.tools import assert_response
+import bin.app as app
+import tests.tools as tools
+
+
+def test_index():
+
+    # check that we get a 404 on the /url
+
+    resp= app.app.request("/")
+    tools.assert_response(resp, status="404")
+
+
+    resp= app.app.request("/hello")
+    tools.assert_response(resp)
+
+    resp= app.app.request("/hello", method="POST")
+    tools.assert_response(resp, contains="Nobody")
+
+    data = {'name': 'Zed', 'greet':'Hola'}
+    resp=app.app.request("/hello", method="POST", data=data)
+    tools.assert_response(resp,contains="Zed")
